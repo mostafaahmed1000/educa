e556d666e74ca3b964667313cd9839664a1f8ec2
mostafa <mostafaa7med1000@gmail.com>
2025-08-04 02:29:44 +0300

    allow cors origin

--- educa/settings/prod.py
+++ educa/settings/prod.py
@@ -10,3 +10,10 @@ ALLOWED_HOSTS = ["*"]
 REDIS_URL = 'redis://cache:6379'
 CACHES['default']['LOCATION'] = REDIS_URL
 CHANNEL_LAYERS['default']['CONFIG']['hosts'] = [REDIS_URL]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",         
+    "http://51.20.193.62/",
+]
+
+CORS_ALLOW_CREDENTIALS = True
